5c423fb0f48c5f7bfaa8e3fb20e1cc3f5bbc6982
Raushan12345comp <rk.777raushan@gmail.com>
2022-02-10 14:30:05 +0530

    first commit

---
+++ src/fragments/auth.ts
@@ -0,0 +1,31 @@
+import { gql } from "@apollo/client";
+
+import { checkoutAddressFragment } from "./checkout";
+
+export const userFragment = gql`
+  ${checkoutAddressFragment}
+  fragment User on User {
+    id
+    email
+    firstName
+    lastName
+    isStaff
+    phone
+    avatar {
+      url
+    }
+    metadata {
+      key
+      value
+    }
+    defaultShippingAddress {
+      ...Address
+    }
+    defaultBillingAddress {
+      ...Address
+    }
+    addresses {
+      ...Address
+    }
+  }
+`;
