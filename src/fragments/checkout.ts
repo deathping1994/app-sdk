10dedc18b3afadb6b8b476b13237bd986444c3ee
Koustav Gandhi <koustavgandhi@Koustavs-MacBook-Air.local>
2025-05-29 14:44:52 +0530

    paymentmethod fields fix

--- src/fragments/checkout.ts
+++ src/fragments/checkout.ts
@@ -193,7 +193,7 @@ export const checkoutFragment = gql`
     }
     paymentMethod {
       cashbackDiscountAmount
-      couponDiscountAmount
+      couponDiscount
       prepaidDiscountAmount
     }
     metadata {
