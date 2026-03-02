2727885c05043b0eabbd692bb0c522c7b1b7398a
Arvind Singh Yadav <arvindsinghyadav@Arvinds-MacBook-Air.local>
2026-03-02 12:28:36 +0530

    customerAttach

--- src/api/Auth/index.ts
+++ src/api/Auth/index.ts
@@ -332,6 +332,11 @@ export class AuthAPI extends ErrorListener {
 
     const { data: userData, dataError: userDataError } =
       await this.jobsManager.run("auth", "provideUser", undefined);
+    if (this.config.loadOnStart.checkout) {
+      await this.jobsManager.run("checkout", "provideCheckout", {
+        isUserSignedIn: true,
+      });
+    }
       
     return {
       data: userData,
